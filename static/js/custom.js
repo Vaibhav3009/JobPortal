d33e869e855c62df03476293607a2cb9a5ca9761
Vaibhav3009 <vaibhavagarwal3009@gmail.com>
2021-05-28 11:16:14 +0530

    Added readme file

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,3 +1,8 @@
+
+$('#collapse').on("click",function(){
+	
+  })
+
 jQuery(function($) {
 
 	'use strict';
@@ -155,18 +160,16 @@ jQuery(function($) {
 	// navigation
   var OnePageNavigation = function() {
     var navToggler = $('.site-menu-toggle');
-   	$("body").on("click", ".main-menu li a[href^='#'], .smoothscroll[href^='#'], .site-mobile-menu .site-nav-wrap li a", function(e) {
-      e.preventDefault();
+   	// $("body").on("click", ".main-menu li a[href^='#'], .smoothscroll[href^='#'], .site-mobile-menu .site-nav-wrap li a", function(e) {
+    //   e.preventDefault();
 
-      var hash = this.hash;
+    //   var hash = this.hash;
 
-      $('html, body').animate({
-        'scrollTop': $(hash).offset().top
-      }, 600, 'easeInOutCirc', function(){
-        window.location.hash = hash;
-      });
+     
+    //     window.location.hash = hash;
+   
 
-    });
+    // });
   };
   OnePageNavigation();
 
